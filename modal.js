c1b313a20cb84f949e96298a40acc145bebeb023
Eduardo Ribeiro <ribeiroedu88@gmail.com>
2021-12-19 23:02:55 -0300

    atual

---
+++ modal.js
@@ -0,0 +1,11 @@
+const openModal = () => {
+    let ovelay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    ovelay.style.display = 'flex'
+}
+
+const closeModal = () => {
+    let ovelay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    ovelay.style.display = 'none'
+}
